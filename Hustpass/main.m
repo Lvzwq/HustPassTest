3c7ef1071a119ad6e1be7f0f7a171e5027bf8cf0
zwenqiang <qiangge@mogujie.org>
2015-10-08 08:27:56 +0800

    Initial Commit

---
+++ Hustpass/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hustpass
+//
+//  Created by zwenqiang on 15/10/8.
+//  Copyright © 2015年 zwenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
